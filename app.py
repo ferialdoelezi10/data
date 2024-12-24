d659cab8dde22bc0193d041b7fde5b1c1a4e2548
ferialdoelezi10 <104211405+ferialdoelezi10@users.noreply.github.com>
2024-12-24 21:33:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -98,8 +98,8 @@ def index():
 
     # Part 2: 3D Globe Visualization
     # Paths to NetCDF and GeoJSON files
-    nc_file_path = "https://docs.google.com/spreadsheets/d/1V4xFI_fHcewCI3UpPGQ4zFWcWEIhDeBQ/edit?usp=drive_link&ouid=114011480270416535431&rtpof=true&sd=true"
-    geojson_file_path = "https://drive.google.com/file/d/1giNNChr0OkFwo_xQLL6LhsXvZWh-s7Cj/view?usp=sharing"
+    nc_file_path = "https://drive.google.com/file/d/1IswWKtiNuSUNT5JxFgAaQ1VfXQL_t0OW/view?usp=drive_link"
+    geojson_file_path = "https://drive.google.com/file/d/1giNNChr0OkFwo_xQLL6LhsXvZWh-s7Cj/view?usp=drive_link"
 
     # Load NetCDF data
     data_nc = Dataset(nc_file_path, mode='r')
